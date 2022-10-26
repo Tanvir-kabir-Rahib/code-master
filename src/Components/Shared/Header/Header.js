85c9404cba723edcaeccc9c0083842b887fb72c2
Tanvir Kabir <tanvir@Tanvirs-MacBook-Pro.local>
2022-10-26 19:15:12 +0600

    layout and active link added

--- src/Components/Shared/Header/Header.js
+++ src/Components/Shared/Header/Header.js
@@ -1,20 +1,36 @@
 import React from 'react';
 import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { FaArrowRight, FaUserAlt } from "react-icons/fa";
 import Image from 'react-bootstrap/Image';
+import logo from '../../../media/picture/logo.png';
+
+
 
 const Header = () => {
     return (
         <Navbar collapseOnSelect expand="lg" bg="light" variant="secondary">
             <Container>
-                <Navbar.Brand href="#home" className='fs-3 fw-semibold'>Code Master</Navbar.Brand>
+                <Image
+                    alt=""
+                    src={logo}
+                    width="75"
+                    height="75"
+                    className="d-inline-block align-top"
+                />
+                <Navbar.Brand href="#home" className='fs-3 fw-semibold'><Link className='text-decoration-none text-info'>Code Master</Link></Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
-                        <span className='fw-semibold ms-4'><NavLink>Courses</NavLink></span>
-                        <span className='fw-semibold ms-4'><NavLink>FAQ</NavLink></span>
-                        <span className='fw-semibold ms-4'><NavLink>Blog</NavLink></span>
+                        <NavLink to="/courses" className={({ isActive }) =>
+                            isActive ? "mx-3 text-decoration-none text-info fw-semibold text-decoration-underline" : "mx-3 fw-semibold"
+                        }>Courses</NavLink>
+                        <NavLink to="/faq" className={({ isActive }) =>
+                            isActive ? "mx-3 text-decoration-none text-info fw-semibold text-decoration-underline" : "mx-3 fw-semibold"
+                        }>FAQ</NavLink>
+                        <NavLink to="/blog" className={({ isActive }) =>
+                            isActive ? "mx-3 text-decoration-none text-info fw-semibold text-decoration-underline" : "mx-3 fw-semibold"
+                        }>Blog</NavLink>
                     </Nav>
                     <Nav>
                         <Nav.Link href="#deets">More deets</Nav.Link>
--- src/Layouts/Courses/CourseLayout.js
+++ src/Layouts/Courses/CourseLayout.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
+import LeftSideNav from '../../Components/Shared/LeftSideNav/LeftSideNav';
 
 const CourseLayout = () => {
     return (
         <div>
-            
+            <LeftSideNav></LeftSideNav>
+            <Outlet></Outlet>
         </div>
     );
 };
